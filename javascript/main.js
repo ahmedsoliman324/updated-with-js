25a01377b0a7c455e7496b8f15854739394776e8
ahmedsoliman324 <157862911+ahmedsoliman324@users.noreply.github.com>
2024-06-06 23:01:26 +0300

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,40 @@
+let main = document.getElementById('main-img');
+let first = document.getElementById('first');
+let second = document.getElementById('sec');
+let third = document.getElementById('thrd');
+let four = document.getElementById('fourth');
+
+
+
+first.onclick = function(){
+    main.src = 'imgs/11.jpg';
+}
+second.onclick =function(){
+    main.src = 'imgs/2.jpg';
+}
+third.onclick = function(){
+    main.src = 'imgs/8-removebg-preview.png';
+}
+four.onclick = function() {
+    main.src = 'imgs/s-l960.jpg'
+}
+
+
+
+//   hover = on mouse enter
+
+
+
+// first.onmouseenter = function(){
+//     main.src = 'imgs/11.jpg';
+// }
+// second.onmouseenter =function(){
+//     main.src = 'imgs/2.jpg';
+// }
+// third.onmouseenter = function(){
+//     main.src = 'imgs/8-removebg-preview.png';
+// }
+// four.onmouseenter = function() {
+//     main.src = 'imgs/s-l960.jpg'
+// }
+
